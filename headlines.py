4e8c67150b4660b9aeae59d8623dc8d76fff1cfa
Peter Shanks <peter@sproutlabs.com.au>
2016-07-14 17:01:28 +1000

    url encode and decode more descriptive multi word keys in RSS_FEEDS

--- headlines.py
+++ headlines.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import urllib.parse
+
 RSS_FEEDS = {'Best of abc.net.au': 'http://abc.net.au/bestof/bestofabc.xml', 'Radio Australia: Asia Pacific': 'http://abc.net.au/ra/rss/asiapacific.rss',
              'ABC iView Programs': 'http://tvmp.abc.net.au/iview/rss/category/abc1.xml', 'ABC Hobart': 'http://www.abc.net.au/local/rss/hobart/news.xml'}
 app = Flask(__name__)
@@ -13,13 +15,11 @@ app = Flask(__name__)
 @app.route("/<publication>")
 def get_news():
     try:
-        query = request.args.get("publication")
-        if not query or query.lower() not in RSS_FEEDS:
-            publication = "ABC Hobart"
-        else:
-            publication = query.lower()
-        feed = feedparser.parse(RSS_FEEDS[publication])
-        return render_template("home.html", articles=feed['entries'], header=publication, rssFeeds= RSS_FEEDS)
+        query = urllib.parse.unquote_plus(request.args.get("publication"))
+        if not query or query not in RSS_FEEDS:
+            query = "ABC Hobart"
+        feed = feedparser.parse(RSS_FEEDS[query])
+        return render_template("home.html", articles=feed['entries'], header=query, rssFeeds= RSS_FEEDS)
     except:
         return "no news is good news"
 
