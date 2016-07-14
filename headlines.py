0b1c4feaf2c6466276ced19c9cd1095bce76864b
Peter Shanks <peter@sproutlabs.com.au>
2016-07-14 15:58:17 +1000

    Using the dictionary to list options, updated to use ABC Australia RSS feeds

--- headlines.py
+++ headlines.py
@@ -4,10 +4,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml', 'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest', 'abc': 'http://www.abc.net.au/local/rss/hobart/news.xml'}
-
+RSS_FEEDS = {'Best of abc.net.au': 'http://abc.net.au/bestof/bestofabc.xml', 'Radio Australia: Asia Pacific': 'http://abc.net.au/ra/rss/asiapacific.rss',
+             'ABC iView Programs': 'http://tvmp.abc.net.au/iview/rss/category/abc1.xml', 'ABC Hobart': 'http://www.abc.net.au/local/rss/hobart/news.xml'}
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/<publication>")
@@ -15,13 +15,13 @@ def get_news():
     try:
         query = request.args.get("publication")
         if not query or query.lower() not in RSS_FEEDS:
-            publication = "abc"
+            publication = "ABC Hobart"
         else:
             publication = query.lower()
         feed = feedparser.parse(RSS_FEEDS[publication])
-        return render_template("home.html",articles=feed['entries'], header=publication.upper())
+        return render_template("home.html", articles=feed['entries'], header=publication, rssFeeds= RSS_FEEDS)
     except:
-        return "no news is good news - try /bbc, /cnn, /fox or /abc for the bad"
+        return "no news is good news"
 
 
 if __name__ == '__main__':
